ba4afa27ae32b2228ae98403a58f5d8a1a4e6750
EmAnt <joshua999x@gmail.com>
2015-09-06 12:16:46 +0200

    Add waitForImages Plugin ta handle preloading

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,24 +12,28 @@
 
 $(document).ready(function() { // makes sure the whole site is loaded
 
-	var timeout = 1500;
-	var bgPos;
+  $('body').waitForImages(function(){
 
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(350).css({'overflow': 'visible'});
+    var timeout = 1500;
+
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({'overflow': 'visible'});
 
     setTimeout(function(){		//wait  then show home
-			$('#info').animate({opacity: 1}, timeout);
-			$('#logo').animate({opacity: 1, top: '0px'}, timeout);
-			$('.links').animate({opacity: 1, left: '0px'}, timeout);
-			$('footer').animate({opacity: 1}, timeout);
-			$('#primary').animate({opacity: 1}, 1.5 * timeout);
-			$('footer').animate({opacity: 1}, 1.5 * timeout);
-			$('#bg').animate({opacity: 1}, timeout);
-		}, timeout);
+      $('#info').animate({opacity: 1}, timeout);
+      $('#logo').animate({opacity: 1, top: '0px'}, timeout);
+      $('.links').animate({opacity: 1, left: '0px'}, timeout);
+      $('footer').animate({opacity: 1}, timeout);
+      $('#primary').animate({opacity: 1}, 1.5 * timeout);
+      $('footer').animate({opacity: 1}, 1.5 * timeout);
+      $('#bg').animate({opacity: 1}, timeout);
+    }, timeout);
+  });
 
-    $('.main-links')
+  var bgPos;
+
+  $('.main-links')
 	.hover(function(){
 		bgPos = $(this).css('background-position').split(' ');
 		$(this).css('background-position', bgPos[0] + ' -48px');
