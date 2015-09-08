55395bb78de418fec839481b3ade9a14d3e8e693
EmAnt <joshua999x@gmail.com>
2015-09-08 14:59:54 +0200

    Configure waitForImage plugin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,23 +12,26 @@
 
 $(document).ready(function() { // makes sure the whole site is loaded
 
-  $('body').waitForImages(function(){
+  $('body').waitForImages({
+    waitForAll: true,
+    finished: function(){
 
-    var timeout = 1500;
+      var timeout = 1500;
 
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({'overflow': 'visible'});
+      $('#status').fadeOut(); // will first fade out the loading animation
+      $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      $('body').delay(350).css({'overflow': 'visible'});
 
-    setTimeout(function(){		//wait  then show home
-      $('#info').animate({opacity: 1}, timeout);
-      $('#logo').animate({opacity: 1, top: '0px'}, timeout);
-      $('.links').animate({opacity: 1, left: '0px'}, timeout);
-      $('footer').animate({opacity: 1}, timeout);
-      $('#primary').animate({opacity: 1}, 1.5 * timeout);
-      $('footer').animate({opacity: 1}, 1.5 * timeout);
-      $('#bg').animate({opacity: 1}, timeout);
-    }, timeout);
+      setTimeout(function(){		//wait  then show home
+        $('#info').animate({opacity: 1}, timeout);
+        $('#logo').animate({opacity: 1, top: '0px'}, timeout);
+        $('.links').animate({opacity: 1, left: '0px'}, timeout);
+        $('footer').animate({opacity: 1}, timeout);
+        $('#primary').animate({opacity: 1}, 1.5 * timeout);
+        $('footer').animate({opacity: 1}, 1.5 * timeout);
+        $('#bg').animate({opacity: 1}, timeout);
+      }, timeout);
+    }
   });
 
   var bgPos;
