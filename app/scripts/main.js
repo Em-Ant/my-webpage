7cf9c63c3b6c9238ecf19c7bb7d715c12aa5bb0e
emant <joshua999x@gmail.com>
2022-07-21 16:57:27 +0200

    format code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,3 @@
-
 /**************************************************************
 ---------------------------------------------------------------
 		Jquery Script for the EmAnt's Personal Webpage
@@ -8,162 +7,192 @@
 
 **************************************************************/
 
-'use strict';
+"use strict";
 
-$(document).ready(function() { // makes sure the whole site is loaded
+$(document).ready(function () {
+  // makes sure the whole site is loaded
 
   function loadPfolio(e) {
-    if(e) {
+    if (e) {
       e.preventDefault();
-      window.location.hash = '#/portfolio';
+      window.location.hash = "#/portfolio";
     }
-    $('#home_cont').fadeOut(800, function(){
-      $('#pfolio-cont').scrollTop(0).fadeIn(600);
+    $("#home_cont").fadeOut(800, function () {
+      $("#pfolio-cont").scrollTop(0).fadeIn(600);
     });
   }
 
   function backHome(e) {
-    if(e) {
+    if (e) {
       e.preventDefault();
-      window.location.hash = '';
+      window.location.hash = "";
     }
-    $('#pfolio-cont').fadeOut(800, function(){
-      $('#home_cont').scrollTop(0).fadeIn(600);
+    $("#pfolio-cont").fadeOut(800, function () {
+      $("#home_cont").scrollTop(0).fadeIn(600);
     });
   }
 
   function checkHash() {
-    if(window.location.hash === '#/portfolio') {
+    if (window.location.hash === "#/portfolio") {
       loadPfolio();
     } else {
-      window.location.hash = '';
+      window.location.hash = "";
       backHome();
     }
   }
 
-  $('body').waitForImages({
+  $("body").waitForImages({
     waitForAll: true,
-    finished: function(){
-
-      if('onhashchange' in window) {
+    finished: function () {
+      if ("onhashchange" in window) {
         window.onhashchange = checkHash;
 
-        if(window.location.hash === '#/portfolio') {
+        if (window.location.hash === "#/portfolio") {
           loadPfolio();
         }
       } else {
-        $('#pfolio_link').click(loadPfolio);
-        $('#back').click(backHome);
+        $("#pfolio_link").click(loadPfolio);
+        $("#back").click(backHome);
       }
 
       var timeout = 1500;
 
-      $('#status').fadeOut(); // will first fade out the loading animation
-      $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-      $('body').delay(350).css({'overflow': 'visible'});
+      $("#status").fadeOut(); // will first fade out the loading animation
+      $("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+      $("body").delay(350).css({ overflow: "visible" });
 
-      setTimeout(function(){		//wait  then show home
-        $('#info').animate({opacity: 1}, timeout);
-        $('#logo').animate({opacity: 1, top: '0px'}, timeout);
-        $('.links').animate({opacity: 1, left: '0px'}, timeout, 'swing', function(){
-          $('#portfolio').animate({opacity: 1}, timeout);
-        });
-        $('footer').animate({opacity: 1}, timeout);
-        $('#primary').animate({opacity: 1}, 1.5 * timeout);
-        $('footer').animate({opacity: 1}, 1.5 * timeout);
-        $('#bg').animate({opacity: 1}, timeout);
-
+      setTimeout(function () {
+        //wait  then show home
+        $("#info").animate({ opacity: 1 }, timeout);
+        $("#logo").animate({ opacity: 1, top: "0px" }, timeout);
+        $(".links").animate(
+          { opacity: 1, left: "0px" },
+          timeout,
+          "swing",
+          function () {
+            $("#portfolio").animate({ opacity: 1 }, timeout);
+          }
+        );
+        $("footer").animate({ opacity: 1 }, timeout);
+        $("#primary").animate({ opacity: 1 }, 1.5 * timeout);
+        $("footer").animate({ opacity: 1 }, 1.5 * timeout);
+        $("#bg").animate({ opacity: 1 }, timeout);
       }, timeout);
-    }
+    },
   });
 
   var bgPos;
-  $('.main-links')
-	.hover(function(){
-		bgPos = $(this).css('background-position').split(' ');
-		$(this).css('background-position', bgPos[0] + ' -48px');
-	}, function(){
-		$(this).css('background-position', bgPos[0] + ' 0');
-	});
+  $(".main-links").hover(
+    function () {
+      bgPos = $(this).css("background-position").split(" ");
+      $(this).css("background-position", bgPos[0] + " -48px");
+    },
+    function () {
+      $(this).css("background-position", bgPos[0] + " 0");
+    }
+  );
 
-  $('#portfolio a')
-    .hover(function(){
-      $(this).css('background-position', '0 -82px');
-    }, function(){
-      $(this).css('background-position', '0 0');
-    });
+  $("#portfolio a").hover(
+    function () {
+      $(this).css("background-position", "0 -82px");
+    },
+    function () {
+      $(this).css("background-position", "0 0");
+    }
+  );
 
+  /**
+   * Contacts Form validation setup
+   */
 
+  var validator = $("#contacts-form").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      message: "required",
+    },
+    errorClass: "has-error",
 
-	/**
-	* Contacts Form validation setup
-	*/
+    validClass: "has-success",
 
-	var validator = $('#contacts-form').validate({
-		rules: {
-			email: {
-				required: true,
-				email: true
-			},
-			message: 'required'
-		},
-		errorClass: 'has-error',
+    highlight: function (el, errClass, validClass) {
+      $("#" + $(el).attr("name") + "-group")
+        .addClass(errClass)
+        .removeClass(validClass);
+    },
 
-		validClass: 'has-success',
+    unhighlight: function (el, errClass, validClass) {
+      $("#" + $(el).attr("name") + "-group")
+        .removeClass(errClass)
+        .addClass(validClass);
+    },
 
-		highlight: function(el, errClass, validClass){
-			$('#' + $(el).attr('name') + '-group').addClass(errClass).removeClass(validClass);
-		},
+    onkeyup: function (element) {
+      $(element).valid();
+    },
+    errorPlacement: function (error, element) {
+      $("#" + $(element).attr("name") + "-err").append(error);
+    },
 
-		unhighlight: function(el, errClass, validClass){
-			$('#' + $(el).attr('name') + '-group').removeClass(errClass).addClass(validClass);
-		},
+    submitHandler: function (form) {
+      $(".modal-footer .btn").prop("disabled", "disabled").addClass("disabled");
+      $("#message-tx")
+        .hide()
+        .append(
+          $('<label class="text-primary">Waiting for Response...</label>')
+        )
+        .fadeIn();
+      var data = {
+        action: "send-mail",
+      };
+      data = $(form).serialize() + "&" + $.param(data);
+      $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "./request.php", //Relative or absolute path to response.php file
+        data: data,
+        success: function () {
+          $("#message-tx")
+            .empty()
+            .append($('<label class="text-success">Message Sent !</label>'));
+          formReset(); // eslint-disable-line no-use-before-define
+          setTimeout(function () {
+            $(".modal-footer .btn")
+              .prop("disabled", "")
+              .removeClass("disabled");
+            $("#message-tx").fadeOut(function () {
+              $(this).empty();
+            });
+          }, 2000);
+        },
+        error: function () {
+          $("#message-tx")
+            .empty()
+            .append(
+              $('<label class="text-danger">Unauthorized Operation !</label>')
+            );
+          setTimeout(function () {
+            $(".modal-footer .btn")
+              .prop("disabled", "")
+              .removeClass("disabled");
+            $("#message-tx").fadeOut(function () {
+              $(this).empty();
+            });
+          }, 2000);
+        },
+      });
+    },
+  });
 
-		onkeyup: function(element) {
-			$(element).valid();
-		},
-		errorPlacement: function(error, element){
-			$('#' + $(element).attr('name') + '-err').append(error);
-		},
+  function formReset() {
+    validator.resetForm();
+    $(".form-group").removeClass("has-error").removeClass("has-success");
+    $("#message-text").val("");
+    $("#email-input").val("");
+  }
 
-		submitHandler: function(form) {
-			$('.modal-footer .btn').prop('disabled', 'disabled').addClass('disabled');
-			$('#message-tx').hide().append($('<label class="text-primary">Waiting for Response...</label>')).fadeIn();
-			var data = {
-				'action': 'send-mail'
-			};
-			data = $(form).serialize() + '&' + $.param(data);
-			$.ajax({
-				type: 'POST',
-				dataType: 'json',
-				url: './request.php', //Relative or absolute path to response.php file
-				data: data,
-				success: function(){
-					$('#message-tx').empty().append($('<label class="text-success">Message Sent !</label>'));
-					formReset(); // eslint-disable-line no-use-before-define
-					setTimeout(function(){
-						$('.modal-footer .btn').prop('disabled', '').removeClass('disabled');
-						$('#message-tx').fadeOut(function(){$(this).empty(); });
-					}, 2000);
-				},
-				error: function(){
-					$('#message-tx').empty().append($('<label class="text-danger">Unauthorized Operation !</label>'));
-					setTimeout(function(){
-						$('.modal-footer .btn').prop('disabled', '').removeClass('disabled');
-						$('#message-tx').fadeOut(function(){$(this).empty(); });
-					}, 2000);
-				}
-			});
-		}
-	});
-
-	function formReset(){
-		validator.resetForm();
-		$('.form-group').removeClass('has-error').removeClass('has-success');
-		$('#message-text').val('');
-		$('#email-input').val('');
-	}
-
-	$('.modal-footer .btn').prop('disabled', '').removeClass('disabled');
-	formReset();
+  $(".modal-footer .btn").prop("disabled", "").removeClass("disabled");
+  formReset();
 });
